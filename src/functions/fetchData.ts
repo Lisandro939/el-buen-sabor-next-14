edbb0d3b141f46f55cf22051f30e9532ef96b5c2
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-11-01 11:40:39 -0300

    Update fetchData.ts

--- src/functions/fetchData.ts
+++ src/functions/fetchData.ts
@@ -1,6 +1,6 @@
 'use server'
 
 export async function FetchData () {
-    const data = await fetch('https://el-buen-sabor-api.onrender.com/api/v1/articulosManufacturados', { cache: 'no-store' }).then(res => res.json())
+    const data = await fetch('https://el-buen-sabor.zeabur.app/api/v1/articulosManufacturados', { cache: 'no-store' }).then(res => res.json())
     return data
 }
