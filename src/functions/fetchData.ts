97f5d2211ba8d6dc71761e0bd0d0fded381f46f3
Lisandro Agustín Fernandez Brandt <89790156+Lisandro939@users.noreply.github.com>
2023-11-07 11:18:31 -0300

    Update fetchData.ts

--- src/functions/fetchData.ts
+++ src/functions/fetchData.ts
@@ -3,6 +3,6 @@
 import { API_URL } from "@/consts/CONSTS"
 
 export async function FetchData () {
-    const data = await fetch(`${API_URL}/articulosManufacturados`, { next: { revalidate: 3600 } }).then(res => res.json())
+    const data = await fetch(`${API_URL}/articulosManufacturados`, { cache: 'no-store' }).then(res => res.json())
     return data
 }
